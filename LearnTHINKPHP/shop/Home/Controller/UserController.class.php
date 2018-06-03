49ab99d0b82301b9788812a5c88dd1e23e337dc9
Lucas <574702476@qq.com>
2018-06-03 22:59:05 +0800

    ThinkPHP的表单自动验证
    $model->create();
    $_validate = array(
        array('username', 'require', '用户名不能为空') ,
    );

--- LearnTHINKPHP/shop/Home/Controller/UserController.class.php
+++ LearnTHINKPHP/shop/Home/Controller/UserController.class.php
@@ -9,7 +9,26 @@ class UserController extends Controller {
 		$this->display();
 	}
 	
+	function register() {
+		
+		if(!empty($_POST)) {
+			//处理注册信息
+			$user = new \Model\UserModel();
+			$data = $user->create();
+			
+			if($data) {
+				$data['favourite'] = implode(',', $_POST['favourite']);
+				$res = $user->add($data);
+				$this->redirect('Index/index');else {
+			}else {
+				$this->assign('errorInfo', $user->getError());
+			}
 
+		}
+		//展示注册页面
+		$this->display();
+
+	}
 	
 }
 
--- LearnTHINKPHP/shop/Model/UserModel.class.php
+++ LearnTHINKPHP/shop/Model/UserModel.class.php
@@ -5,6 +5,30 @@ use Think\Model;
 
 class UserModel extends Model {
 	
+	protected $patchValidate = true;
+	
+	protected $_validate = array(
+	
+		array('username', 'require', '用户名不能为空'),
+		array('username', '', '用户名已存在', 0, 'unique'),
+		
+		array('password', 'require', '密码不能为空'),
+		array('password2', 'require', '验证密码不能为空'),
+		array('password', 'password2', '两次密码不一致', 0, 'confirm'),
+		
+		array('email', 'email', '邮箱不符合规范'),
+		
+		array('favourite', 'check_favourite', '爱好必须选择一项或以上', 1, 'callback'),
+	
+	);
+	
+	function check_favourite($arg) {
+		if(count($arg) < 2) {
+			return false;
+		}
+		return true;
+	}
+	
 }
 
 ?>
